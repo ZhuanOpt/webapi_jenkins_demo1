5e482b95c399f013e7c3e6780824a6f38262101b
zhuanyan <zhuanyan1994@gmail.com>
2023-02-15 17:11:09 +0800

    add demo

---
+++ WebApi_Jenkins_demo1/Controllers/DemoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_Jenkins_demo1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return DateTime.Now.ToString("yyyyMMddHHmmssff");
+        }
+
+
+        [HttpGet("{id}")]
+        public string Get(string id)
+        {
+            return $"{id}:-> {DateTime.Now:yyyyMMddHHmmssff}";
+        }
+    }
+}
